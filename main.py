5f285aa4cd9ce71ebdf375efa78656061fbb4091
asadamiruddin <asad.amiruddin@yahoo.com>
2022-12-26 16:18:28 -0500

    reorder columns

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     df = pd.read_csv('top_skills.csv')
+    df = df.iloc[: , 1:]
+    df.confidence_level = df.confidence_level.round(2)
     data = list(df.itertuples(index=False, name=None))
     return render_template('index.html', value = data)
 
